ba45c3b7ad0eb32826707b018cd35346301951aa
Ruby Choiu <12130906@me.mcu.edu.tw>
2026-04-29 18:24:18 +0800

    feat(product-page): 刪除產品功能實作

--- src/app/product-page/product-page.component.ts
+++ src/app/product-page/product-page.component.ts
@@ -52,6 +52,13 @@ export class ProductPageComponent implements OnInit {
   onEdit(product: Product): void {
     this.router.navigate(['product', 'form', product.id]);
   }
+
+  protected onRemove({ id }: Product): void {
+    this.productService.remove(id);
+    this.pageIndex.set(1);
+    this.getProducts(this.pageIndex(), this.pageSize());
+  }
+
   onView(product: Product): void {
     this.router.navigate(['product', 'view', product.id]);
   }
