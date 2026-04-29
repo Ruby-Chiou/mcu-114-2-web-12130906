51dd3e104c7bc0d29712e2674c9f59ade3dd4ed3
Ruby Choiu <12130906@me.mcu.edu.tw>
2026-04-29 18:19:50 +0800

    feat(product-page): 新增產品功能實作

--- src/app/product-page/product-page.component.ts
+++ src/app/product-page/product-page.component.ts
@@ -1,4 +1,4 @@
-import { Component, effect, inject, signal } from '@angular/core';
+import { Component, effect, inject, OnInit, signal } from '@angular/core';
 import { Router } from '@angular/router';
 import { ProductCardListComponent } from '../product-card-list/product-card-list.component';
 import { Product } from '../model/product';
@@ -11,7 +11,7 @@ import { PaginationComponent } from '../pagination/pagination.component';
   templateUrl: './product-page.component.html',
   styleUrl: './product-page.component.scss',
 })
-export class ProductPageComponent {
+export class ProductPageComponent implements OnInit {
   private readonly router = inject(Router);
 
   private readonly productService = inject(ProductService);
@@ -31,11 +31,27 @@ export class ProductPageComponent {
       this.getProducts(pageIndex, pageSize);
     });
   }
+  ngOnInit(): void {
+    throw new Error('Method not implemented.');
+  }
+
+  protected onAdd(): void {
+    const product = new Product({
+      name: '書籍 Z',
+      authors: ['作者甲', '作者乙', '作者丙'],
+      company: '博碩文化',
+      isShow: true,
+      photoUrl: 'https://api.fnkr.net/testimg/200x200/DDDDDD/999999/?text=img',
+      createDate: new Date('2025/4/9'),
+      price: 10000,
+    });
+    this.productService.add(product);
+    this.getProducts(this.pageIndex(), this.pageSize());
+  }
 
   onEdit(product: Product): void {
     this.router.navigate(['product', 'form', product.id]);
   }
-
   onView(product: Product): void {
     this.router.navigate(['product', 'view', product.id]);
   }
